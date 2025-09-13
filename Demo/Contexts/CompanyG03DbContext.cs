01ac6e46a548d0a6c3595e1e71ee1f17fac0a3b2
Mahmoud Mansour <mansour@gmail.com>
2025-09-13 18:34:32 +0300

    Complete Demo

---
+++ Demo/Contexts/CompanyG03DbContext.cs
@@ -0,0 +1,27 @@
+﻿using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Contexts
+{
+    internal class CompanyG03DbContext : DbContext
+    {
+        public CompanyG03DbContext():base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source = .;initial Catalog = CompcanyG03; integrated Security = True;");
+            optionsBuilder.UseSqlServer("Server = .; Database = CompanyG03Db; Trusted_Connection = True; TrustServerCertificate = True;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Demo
+﻿using Demo.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            //CompanyG03DbContext dbContext = new CompanyG03DbContext();
+
+            //try
+            //{
+            //    // Some Code
+            //} 
+            //finally
+            //{
+            //    dbContext.Dispose();
+            //}
+
+            //using (CompanyG03DbContext dbContext = new CompanyG03DbContext())
+            //{
+            //    dbContext.Database.EnsureCreated();
+            //}
+
+            using CompanyG03DbContext dbContext = new CompanyG03DbContext();
+
+            //dbContext.Database.EnsureCreated();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.Migrate();
         }
     }
 }
